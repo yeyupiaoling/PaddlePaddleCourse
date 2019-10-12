4d690d1016577225003c44d03bd3312f9455b9fc
yeyupiaoling <yeyupiaoling@foxmail.com>
2019-10-12 17:27:08 +0800

    add course15 android project

---
+++ course15/app/src/main/java/com/yeyupiaoling/lite/MainActivity.java
@@ -0,0 +1,190 @@
+package com.yeyupiaoling.lite;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.baidu.paddle.lite.MobileConfig;
+import com.baidu.paddle.lite.PaddlePredictor;
+import com.baidu.paddle.lite.Tensor;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private String model_path;
+    // 模型文件夹
+    String assets_path = "model";
+    private boolean load_result = false;
+    // 输入图片的形状，分别是：batch size、通道数、宽度、高度
+    private long[] ddims = {1, 3, 224, 224};
+    private ImageView imageView;
+    private TextView showTv;
+    private PaddlePredictor paddlePredictor;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        model_path = getCacheDir().getAbsolutePath() + File.separator + "model";
+        // 初始化控件
+        initView();
+        // 动态请求权限
+        requestPermissions();
+        // 从assets中复制模型文件到缓存目录下
+        Utils.copyFileFromAsset(this, assets_path, model_path);
+    }
+
+    // 初始化控件
+    private void initView(){
+        Button loadBtn = findViewById(R.id.load);
+        Button clearBtn = findViewById(R.id.clear);
+        Button inferBtn = findViewById(R.id.infer);
+        showTv = findViewById(R.id.show);
+        imageView = findViewById(R.id.image_view);
+
+        final MobileConfig config = new MobileConfig();
+        config.setModelDir(model_path);
+        config.setThreads(2);
+
+        // 加载模型点击事件
+        loadBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                paddlePredictor = PaddlePredictor.createPaddlePredictor(config);
+                if (paddlePredictor != null) {
+                    load_result = true;
+                    Toast.makeText(MainActivity.this, "模型加载成功", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "模型加载失败", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        // 清空模型点击事件
+        clearBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                paddlePredictor = null;
+                load_result = false;
+                Toast.makeText(MainActivity.this, "模型已清空", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // 打开相册选择图片预测点击事件
+        inferBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (load_result){
+                    Intent intent = new Intent(Intent.ACTION_PICK);
+                    intent.setType("image/*");
+                    startActivityForResult(intent, 1);
+                } else {
+                    Toast.makeText(MainActivity.this, "模型未加载", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+
+    // 回调事件
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        String image_path;
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == 1) {
+                if (data == null) {
+                    return;
+                }
+                // 获取相册返回的URI
+                Uri image_uri = data.getData();
+                // 根据图片的URI获取绝对路径
+                image_path = Utils.getPathFromURI(MainActivity.this, image_uri);
+                // 压缩图片用于显示
+                Bitmap bitmap = Utils.getScaleBitmap(image_path);
+                imageView.setImageBitmap(bitmap);
+                // 开始预测图片
+                predictImage(image_path);
+            }
+        }
+    }
+
+
+    // 根据图片的路径预测图片
+    private void predictImage(String image_path) {
+        // 把图片进行压缩
+        Bitmap bmp = Utils.getScaleBitmap(image_path);
+        // 把图片转换成浮点数组，用于预测
+        float[] inputData = Utils.getScaledMatrix(bmp, (int)ddims[2], (int)ddims[3], (int)ddims[1]);
+        try {
+            long start = System.currentTimeMillis();
+            // 执行预测，获取预测结果
+            Tensor input = paddlePredictor.getInput(0);
+            input.resize(ddims);
+            input.setData(inputData);
+            paddlePredictor.run();
+            float[] result = paddlePredictor.getOutput(0).getFloatData();
+            long end = System.currentTimeMillis();
+            // 获取概率最大的标签
+            int r = Utils.getMaxResult(result);
+            // 获取标签对应的类别名称
+            String[] names = {"苹果", "哈密瓜", "胡萝卜", "樱桃", "黄瓜", "西瓜"};
+            String show_text = "标签：" + r + "\n名称：" + names[r] + "\n概率：" + result[r] + "\n时间：" + (end - start) + "ms";
+            // 显示预测结果
+            showTv.setText(show_text);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // 多权限动态申请
+    private void requestPermissions() {
+        List<String> permissionList = new ArrayList<>();
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.READ_EXTERNAL_STORAGE);
+        }
+
+        if (!permissionList.isEmpty()) {
+            ActivityCompat.requestPermissions(this, permissionList.toArray(new String[permissionList.size()]), 1);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1) {
+            if (grantResults.length > 0) {
+                for (int i = 0; i < grantResults.length; i++) {
+
+                    int grantResult = grantResults[i];
+                    if (grantResult == PackageManager.PERMISSION_DENIED) {
+                        String s = permissions[i];
+                        Toast.makeText(this, s + " permission was denied", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ course15/app/src/main/java/com/yeyupiaoling/lite/Utils.java
@@ -0,0 +1,158 @@
+package com.yeyupiaoling.lite;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+
+import static android.graphics.Color.blue;
+import static android.graphics.Color.green;
+import static android.graphics.Color.red;
+
+public class Utils {
+
+    // 获取预测值中最大概率的标签
+    public static int getMaxResult(float[] result) {
+        float probability = result[0];
+        int r = 0;
+        for (int i = 0; i < result.length; i++) {
+            if (probability < result[i]) {
+                probability = result[i];
+                r = i;
+            }
+        }
+        return r;
+    }
+
+    // 对将要预测的图片进行预处理
+    public static float[] getScaledMatrix(Bitmap image, int desWidth, int desHeight, int channels) {
+//        Bitmap rgbaImage = image.copy(Bitmap.Config.ARGB_8888, true);
+//        Bitmap scaleImage = Bitmap.createScaledBitmap(rgbaImage, desWidth, desHeight, true);
+//        int[] channelStride = new int[]{desWidth * desHeight, desWidth * desHeight * 2};
+//        float[] inputData = new float[channels * desWidth * desHeight];
+//        for (int y = 0; y < desHeight; y++) {
+//            for (int x = 0; x < desWidth; x++) {
+//                int color = inputImage.getPixel(x, y);
+//                float[] rgb = new float[]{(float) red(color) / 255.0f, (float) green(color) / 255.0f,
+//                        (float) blue(color) / 255.0f};
+//                inputData[y * width + x] = (rgb[channelIdx[0]] - inputMean[0]) / inputStd[0];
+//                inputData[y * width + x + channelStride[0]] = (rgb[channelIdx[1]] - inputMean[1]) / inputStd[1];
+//                inputData[y * width + x + channelStride[1]] = (rgb[channelIdx[2]] - inputMean[2]) / inputStd[2];
+//            }
+//        }
+//
+
+        float[] dataBuf = new float[channels * desWidth * desHeight];
+        int rIndex;
+        int gIndex;
+        int bIndex;
+        int[] pixels = new int[desWidth * desHeight];
+        Bitmap bm = Bitmap.createScaledBitmap(image, desWidth, desHeight, false);
+        bm.getPixels(pixels, 0, desWidth, 0, 0, desWidth, desHeight);
+        int j = 0;
+        int k = 0;
+        for (int i = 0; i < pixels.length; i++) {
+            int clr = pixels[i];
+            j = i / desHeight;
+            k = i % desWidth;
+            rIndex = j * desWidth + k;
+            gIndex = rIndex + desHeight * desWidth;
+            bIndex = gIndex + desHeight * desWidth;
+            // 转成RGB通道顺序，并除以255，跟训练的预处理一样
+            dataBuf[rIndex] = (float) (((clr & 0x00ff0000) >> 16) / 255.0);
+            dataBuf[gIndex] = (float) (((clr & 0x0000ff00) >> 8) / 255.0);
+            dataBuf[bIndex] = (float) (((clr & 0x000000ff)) / 255.0);
+
+        }
+        if (bm.isRecycled()) {
+            bm.recycle();
+        }
+        return dataBuf;
+    }
+
+    // 压缩图片，避免图片过大
+    public static Bitmap getScaleBitmap(String filePath) {
+        BitmapFactory.Options opt = new BitmapFactory.Options();
+        opt.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(filePath, opt);
+
+        int bmpWidth = opt.outWidth;
+        int bmpHeight = opt.outHeight;
+
+        int maxSize = 500;
+
+        // compress picture with inSampleSize
+        opt.inSampleSize = 1;
+        while (true) {
+            if (bmpWidth / opt.inSampleSize < maxSize || bmpHeight / opt.inSampleSize < maxSize) {
+                break;
+            }
+            opt.inSampleSize *= 2;
+        }
+        opt.inJustDecodeBounds = false;
+        return BitmapFactory.decodeFile(filePath, opt);
+    }
+
+
+    // 根据相册返回的URI返回图片的绝对路径
+    public static String getPathFromURI(Context context, Uri uri) {
+        String result;
+        Cursor cursor = context.getContentResolver().query(uri, null, null, null, null);
+        if (cursor == null) {
+            result = uri.getPath();
+        } else {
+            cursor.moveToFirst();
+            int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+            result = cursor.getString(idx);
+            cursor.close();
+        }
+        return result;
+    }
+
+
+    // 复制莫模型文件到缓存目录
+    public static void copyFileFromAsset(Context context, String oldPath, String newPath) {
+        try {
+            // 预测模型文件在assets中的位置
+            String[] fileNames = context.getAssets().list(oldPath);
+            if (fileNames.length > 0) {
+                // directory
+                File file = new File(newPath);
+                if (!file.exists()) {
+                    file.mkdirs();
+                }
+                // copy recursivelyC
+                for (String fileName : fileNames) {
+                    copyFileFromAsset(context, oldPath + "/" + fileName, newPath + "/" + fileName);
+                }
+            } else {
+                // file
+                File file = new File(newPath);
+                // if file exists will never copy
+                if (file.exists()) {
+                    return;
+                }
+
+                // copy file to new path
+                InputStream is = context.getAssets().open(oldPath);
+                FileOutputStream fos = new FileOutputStream(file);
+                byte[] buffer = new byte[1024];
+                int byteCount;
+                while ((byteCount = is.read(buffer)) != -1) {
+                    fos.write(buffer, 0, byteCount);
+                }
+                fos.flush();
+                is.close();
+                fos.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
